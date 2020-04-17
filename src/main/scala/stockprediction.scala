604c8a16c7751c7631b4649aaa6c96262a65f6e3
siddhusalvi <siddheshsalvi525@gmail.com>
2020-04-17 17:59:53 +0530

    ADDED : Exception handling

--- src/main/scala/stockprediction.scala
+++ src/main/scala/stockprediction.scala
@@ -1,16 +1,19 @@
-import org.apache.spark._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming._
-import org.apache.spark.streaming.StreamingContext._
+
 object stockprediction {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.builder().appName("streamingapp").master("local").getOrCreate()
-    //val stm = new StreamingContext(spark.sparkContext,Seconds(1))
-    val arr = Array("1600 7000 7100 7700")
-    arr.foreach(println(_))
-    val line = spark.sparkContext.parallelize(arr)
-    val command = "python C:\\Users\\Siddesh\\Downloads\\py_predictor.py"
-    val op = line.pipe(command).collect()
-    op.foreach(println(_))
+    try {
+      val spark = SparkSession.builder().appName("streamingapp").master("local").getOrCreate()
+      val stm = new StreamingContext(spark.sparkContext, Seconds(1))
+      val arr = Array("1600 7000 7100 7700")
+      arr.foreach(println(_))
+      val line = spark.sparkContext.parallelize(arr)
+      val command = "python C:\\Users\\Siddesh\\Downloads\\py_predictor.py"
+      val op = line.pipe(command).collect()
+      op.foreach(println(_))
+    } catch {
+      case _ => println("Unknown Error occured!")
+    }
   }
 }
