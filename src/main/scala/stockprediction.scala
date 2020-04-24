25694cf0ec5cc0d1b51c20eef7fc24d339b627b6
siddhusalvi <siddheshsalvi525@gmail.com>
2020-04-24 20:51:15 +0530

    REFACTORED : Exception handling

--- src/main/scala/stockprediction.scala
+++ src/main/scala/stockprediction.scala
@@ -1,54 +1,56 @@
 import org.apache.hadoop.fs.{FileSystem, Path}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
-import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 object stockprediction extends Serializable {
   def main(args: Array[String]): Unit = {
-//    try {
+    try {
       val spark = SparkSession.builder().master("local[2]").appName("StreamingApp").getOrCreate()
       val sc = spark.sparkContext
       val stm = new StreamingContext(sc, Seconds(20))
       val line = stm.socketTextStream("localhost", 9999)
-      var command = "python C:\\Users\\Siddesh\\Downloads\\py_predictor.py"
-//      val command = "python C:\\Users\\Siddesh\\Desktop\\normal.py"
-      var flag = true
+
+      //Python files
+      val path = "C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources\\"
+
+      //Path to save file
+      val path1 = "C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources"
+
+      var command = "python " + path + "py_predictor.py"
       // Create FileSystem object from Hadoop Configuration
       val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
       line.foreachRDD(rdd => {
-        if (!rdd.partitions.isEmpty && flag)
-          rdd.saveAsTextFile("C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources")
+        if (!rdd.partitions.isEmpty)
+          rdd.saveAsTextFile(path1)
 
         val fileExists = fs.exists(new Path("C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources\\part-00000"))
         if (fileExists) {
-          val file = Source.fromFile("C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources\\part-00000").getLines()
-          var lst:String = ""
-          for (line <- file){
-            lst += line
+          val file = Source.fromFile(path + "part-00000").getLines()
+          var file_data: String = ""
+          for (line <- file) {
+            file_data += line
           }
           print("list : ")
-          command += " "+lst
+          command += " " + file_data
           print(command)
-          var dst :List[String]= List(lst)
+          var dst: List[String] = List(file_data)
           //print(lst.mkString(" ")+"|")
           val dta = sc.parallelize(dst)
           val op = dta.pipe(command)
           val output = op.collect()
           println("python output : ")
           output.foreach(println(_))
-          flag = false
-          stm.awaitTerminationOrTimeout(0)
         }
       })
       line.print()
       stm.start()
       stm.awaitTermination()
-//    } catch {
-//      case exception1: NoSuchMethodError => println(exception1)
-//      case exception2: ClassNotFoundException => println(exception2)
-//      case _ => println("Unknown Error occured!")
-//    }
+    } catch {
+      case exception1: NoSuchMethodError => println(exception1)
+      case exception2: ClassNotFoundException => println(exception2)
+      case _ => println("Unknown Error occured!")
+    }
   }
 }
 //115.1600 115.7000 114.7100 114.7700
