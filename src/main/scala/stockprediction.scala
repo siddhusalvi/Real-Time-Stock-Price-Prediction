61a12566ea0f68750d8c336959de5cf5074636ff
siddhusalvi <siddheshsalvi525@gmail.com>
2020-04-27 12:14:05 +0530

    REFACTORED : Executiong file using CLI

--- src/main/scala/stockprediction.scala
+++ src/main/scala/stockprediction.scala
@@ -3,57 +3,58 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import scala.io.Source
 
-object stockprediction extends Serializable {
+object stockprediction {
   def main(args: Array[String]): Unit = {
-//    try {
+    try {
       val spark = SparkSession.builder().master("local[2]").appName("StreamingApp").getOrCreate()
       val sc = spark.sparkContext
       val stm = new StreamingContext(sc, Seconds(20))
       val line = stm.socketTextStream("localhost", 9999)
 
-      //Python files
-      val path = "C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources\\"
+      //Resource  files
+      val path = "C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources"
+      val script = "py_predictor.py"
+      val spark_op = "part-00000"
+      var command = "python " + path + "\\" + script
 
-      //Path to save file
-      val path1 = "C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources"
-
-      var command = "python " + path + "py_predictor.py"
       // Create FileSystem object from Hadoop Configuration
       val fs = FileSystem.get(spark.sparkContext.hadoopConfiguration)
+
       line.foreachRDD(rdd => {
         if (!rdd.partitions.isEmpty)
-          rdd.saveAsTextFile(path1)
+          rdd.saveAsTextFile(path + "\\")
 
-        val fileExists = fs.exists(new Path("C:\\Users\\Siddesh\\IdeaProjects\\streamingdata\\src\\main\\resources\\part-00000"))
+        val fileExists = fs.exists(new Path(path + "\\"+spark_op))
         if (fileExists) {
-          val file = Source.fromFile(path + "part-00000").getLines()
-          var file_data: String = ""
-          for (line <- file) {
-            file_data += line
-          }
-//          val vector = file_data.split(" ").toList
-//          println("list is  ")
-//          println(vector.mkString(" "))
-//          print("list : ")
-//          command += " " + file_data
-//          print(command)
-          val dst = Array(file_data)
-          //print(lst.mkString(" ")+"|")
-          val dta = sc.parallelize(dst)
-          val op = dta.pipe(command)
-          val output = op.collect()
-          println("python output : ")
+          var file_data = getFileContent(path + "\\" + spark_op)
+          command  += " " + file_data
+          val list = Array(" ")
+          val operation = sc.parallelize(list)
+          val std_op = operation.pipe(command)
+          val output = std_op.collect()
           output.foreach(println(_))
         }
       })
       line.print()
       stm.start()
       stm.awaitTermination()
-//    } catch {
-//      case exception1: NoSuchMethodError => println(exception1)
-//      case exception2: ClassNotFoundException => println(exception2)
-//      case _ => println("Unknown Error occured!")
-//    }
+    } catch {
+      case exception1: NoSuchMethodError => println(exception1)
+      case exception2: ClassNotFoundException => println(exception2)
+      case exception3: InterruptedException => println(exception3)
+      case _ => println("Unknown Error occured!")
+    }
+  }
+  //function to get file contents
+  def getFileContent(path: String):String={
+    val file = Source.fromFile(path).getLines()
+    var file_data: String = ""
+    for (line <- file) {
+      file_data += line
+    }
+    file_data
   }
 }
-//115.1600 115.7000 114.7100 114.7700
+
+
+
